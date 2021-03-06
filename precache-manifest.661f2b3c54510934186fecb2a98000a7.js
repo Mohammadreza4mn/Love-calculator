568ca637debb07230f2f30da6b86fcc0500831d4
MohammadReza <md.mohammadreza+git@gmail.com>
2021-03-06 11:09:09 +0330

    Updates

---
+++ precache-manifest.661f2b3c54510934186fecb2a98000a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12690f74169a513ffe4993f21e045027",
+    "url": "/Love-calculator/index.html"
+  },
+  {
+    "revision": "4e93eb6349de2c6d828d",
+    "url": "/Love-calculator/static/css/main.bdaab0f8.chunk.css"
+  },
+  {
+    "revision": "0a3ab53b8cce37a81435",
+    "url": "/Love-calculator/static/js/2.0db10d1c.chunk.js"
+  },
+  {
+    "revision": "1fab76129d57290d93b752be1cdae21d",
+    "url": "/Love-calculator/static/js/2.0db10d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714c2c2baedcec5622df",
+    "url": "/Love-calculator/static/js/3.c4984c96.chunk.js"
+  },
+  {
+    "revision": "4e93eb6349de2c6d828d",
+    "url": "/Love-calculator/static/js/main.f0c73500.chunk.js"
+  },
+  {
+    "revision": "ff3dcea5a70e521860e2",
+    "url": "/Love-calculator/static/js/runtime-main.2acc7a43.js"
+  },
+  {
+    "revision": "071f0c0d334fd089d6cedc0190f28873",
+    "url": "/Love-calculator/static/media/Vazir.071f0c0d.ttf"
+  },
+  {
+    "revision": "5b3e274fcc8f9e1f0a1ddd65bb380676",
+    "url": "/Love-calculator/static/media/Vazir.5b3e274f.eot"
+  },
+  {
+    "revision": "75d92f56c0d5b8d89c22ce702e442c45",
+    "url": "/Love-calculator/static/media/Vazir.75d92f56.woff2"
+  },
+  {
+    "revision": "b72f4d539c98c12b49f34f3c7d054d72",
+    "url": "/Love-calculator/static/media/Vazir.b72f4d53.woff"
+  },
+  {
+    "revision": "3efcfd6462ebe757a5c6ddbb83bf3612",
+    "url": "/Love-calculator/static/media/m.m.3efcfd64.jpg"
+  }
+]);
